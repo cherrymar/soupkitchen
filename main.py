6750463fa2583552f2aa65eafff40a7aa8fa91a2
cherrymar <cherma88@hotmail.com>
2018-08-02 13:26:59 -0700

    main page setup

--- main.py
+++ main.py
@@ -6,5 +6,31 @@ import math
 import logging
 from google.appengine.ext import ndb
 from google.appengine.api import users
-from models import
+# from models import
 import base64
+
+
+jinja_current_directory = jinja2.Environment(
+    loader=jinja2.FileSystemLoader( os.path.dirname(__file__) ),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
+
+
+class Main_Page( webapp2.RequestHandler ):
+    def get( self ):
+        
+
+
+        start_template = jinja_current_directory.get_template("templates/main_page.html")
+        self.response.write( start_template.render())
+
+
+
+
+
+
+#route mapping
+app = webapp2.WSGIApplication([
+    ('/', Main_Page),
+
+], debug=True)
