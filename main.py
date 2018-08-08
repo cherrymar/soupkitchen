765b0d06f5eb729f1bb66da51c20d44317b19cd6
cherrymar <cherma88@hotmail.com>
2018-08-08 12:00:14 -0700

    added to css and models mainly

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import math
 import logging
 from google.appengine.ext import ndb
 from google.appengine.api import users
-# from models import
+from models import Restuarant
 import base64
 
 
@@ -18,11 +18,35 @@ jinja_current_directory = jinja2.Environment(
 
 class Main_Page( webapp2.RequestHandler ):
     def get( self ):
-        
+        current_user = users.get_current_user()
+        if not current_user:
+            template = jinja_current_directory.get_template("templates/main_page.html")
+            jinja_values = {
+            'signin_page_url' : users.create_login_url('/'),
+            }
+            self.response.write( template.render( jinja_values ))
+        else:
+            #datastore
+            current_key = ndb.Key('Restaurant', current_user.user_id() )
+            current_restaurant = current_key.get()
+            # if not current_visitor:
+                # current_Restaurant = Restaurant(key=current_key, name=current_user.nickname(), email=current_user.email(), id=current_user.user_id() )
+            # current_visitor.put()
+
+            #display
+            template = jinja_current_directory.get_template("templates/main_page.html")
+            # jinja_values = {
+            # 'name' : current_user.nickname(),
+            # 'user_addr' : current_user.email(),
+            # 'user_id' : current_user.user_id(),
+            # 'signout_page_url' : users.create_logout_url('/'),
+            # }
+            self.response.write( template.render())
 
 
-        start_template = jinja_current_directory.get_template("templates/main_page.html")
-        self.response.write( start_template.render())
+
+        # start_template = jinja_current_directory.get_template("templates/main_page.html")
+        # self.response.write( start_template.render())
 
 
 
--- models.py
+++ models.py
@@ -4,19 +4,24 @@ class Restuarant(ndb.Model):
     name = ndb.StringProperty(required=False)
     location = ndb.StringProperty(required=False)
     id = ndb.IntegerProperty(required=False)
-    num_servings = ndb.IntegerProperty(required=False)
-    types_of_food = ndb.StringProperty(repeated=False)
+    foods_available = ndb.StringProperty(repeated=True) #save id's of food here
+    password = ndb.StringProperty(required=True)
 
-class Food(object):
-    def __init__(self):
-        self.name = name
-        self.ingredients = ingredients
-        
+
+class Food(ndb.Model):
+    id = ndb.IntegerProperty(required=False)
+    name = ndb.StringProperty(required=False)
+    ingredients = ndb.StringProperty(repeated=True)
 
 
 class Shelter(ndb.Modle):
-    name =
-    location =
-    num_occupants =
-    num_chosen =
-    reservations =
+    name = ndb.StringProperty(required=False)
+    location = ndb.StringProperty(required=False)
+    occupants = ndb.StringProperty(repeated=True) #save id's of people here
+
+
+class People(ndb.Model):
+    id = ndb.IntegerProperty(required=False)
+    name = ndb.StringProperty(required=False)
+    age = ndb.IntegerProperty(required=False)
+    weight = ndb.IntegerProperty(required=False)
